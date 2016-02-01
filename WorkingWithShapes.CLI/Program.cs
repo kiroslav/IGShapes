05ca0b0cdbe1154815b9bc31e0698449f37d3516
Brian Wint <BWint@infragistics.com>
2016-02-01 15:47:13 -0500

    Initial source commit

---
+++ WorkingWithShapes.CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorkingWithShapes.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The length of the side is " + args[0]);
+            int shapeLength = int.Parse(args[0]);
+            ShapesBasics.Square mySquare = new ShapesBasics.Square();
+            mySquare.Length = shapeLength;
+            Console.WriteLine("The area is " + mySquare.CalculateArea().ToString());
+        }
+    }
+}
---
+++ WorkingWithShapes.Tests/CalculateAreaTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkingWithShapes.Tests
+{
+    [TestClass]
+    public class CalculateAreaTests
+    {
+        [TestMethod]
+        public void CalculateArea()
+        {
+            ShapesBasics.Square testSquare = new ShapesBasics.Square();
+            testSquare.Length = 5;
+            int expectedAreaValue = 25;
+            int actualAreaValue = testSquare.CalculateArea();
+
+            Assert.AreEqual(expectedAreaValue, actualAreaValue);
+        }
+
+        [TestMethod]
+        public void CalculateAreaIsNotEqual()
+        {
+            ShapesBasics.Square testSquare = new ShapesBasics.Square();
+            testSquare.Length = 6;
+            int expectedAreaValue = 25;
+            int actualAreaValue = testSquare.CalculateArea();
+
+            Assert.AreNotEqual(expectedAreaValue, actualAreaValue);
+        }
+
+        [TestMethod]
+        public void DoTwoSquaresHaveEqualArea()
+        {
+            ShapesBasics.Square testSquare1 = new ShapesBasics.Square();
+            testSquare1.Length = 5;
+
+            ShapesBasics.Square testSquare2 = new ShapesBasics.Square();
+            testSquare1.Length = 5;
+
+            Assert.AreEqual(testSquare1, testSquare2);
+        }
+    }
+}
