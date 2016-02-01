6e5738b346bf80f7ec5e32b28a7cf525eb209963
Brian Wint <BWint@infragistics.com>
2016-02-01 15:58:31 -0500

    Initial Source Commit

---
+++ ShapesBasics/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShapesBasics
+{
+    public class Square
+    {
+        public int Length;
+
+        public int CalculateArea()
+        {
+            return Length * Length;
+        }
+    }
+}
